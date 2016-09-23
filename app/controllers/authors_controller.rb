84756aa2c2b2ac0d4eb0d2735148fb4c5b65cff2
fidelscodes <fidelscodes@gmail.com>
2016-09-23 11:05:20 -0400

    done

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -4,4 +4,20 @@ class AuthorsController < ApplicationController
     @author = Author.find(params[:id])
   end
 
+#   def posts_index
+#     @author = Author.find(params[:id])
+#     @posts = @author.posts
+#     render template: 'posts/index'
+#   end
+
+#   def post
+#     @auhtor = Author.find(params[:id])
+#     # Note that because ids are unique by table, we can go direclty to Post.find
+#     # No need for @author.posts.find...
+#     @post = Post.find(params[:id])
+#     # This works just as well without the key of template:
+#     render 'posts/show'
+
+    # rendering posts this way isn't really the responsibility of the authors_controller, though...
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,16 @@
 Rails.application.routes.draw do
 
-  resources :authors, only: [:show]
+  resources :authors, only: [:show] do
+    # Nested resources for posts
+    resources :posts, only: [:show, :index]
+  end
 
   resources :posts, only: [:index, :show, :new, :create, :edit, :update]
 
   root 'posts#index'
+
+  # Have to tell these routes which controller actions will handle them
+  # Then we also have to make sure to update the authors_controller with the appropriate actions
+  # get 'authors/:id/posts', to: 'authors#posts_index'
+  # get 'authors/:id/posts/:post_id', to: 'authors#post'
 end
